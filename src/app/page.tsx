917692b6e321cbdee3a615e653e083f566b10319
echo <play3step@naver.com>
2025-09-18 15:31:29 +0900

    [chore] tailwind 색상 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import { tw } from '@/shared/lib'
 export default function Home() {
   return (
     <div
-      className={tw(
-        'flex flex-col items-center justify-center h-screen text-red-600 text-2xl'
-      )}>
+      className={tw('flex-center text-green-normal h-screen text-g text-2xl')}>
       <h1>Hello World</h1>
     </div>
   )
