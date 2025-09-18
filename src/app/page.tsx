340a183ae529a2db08f845d46735dc1f26f6f91b
echo <play3step@naver.com>
2025-09-18 14:35:43 +0900

    [chore] tanstack, zustand 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { tw } from '@/shared/lib'
+
 export default function Home() {
   return (
-    <div>
+    <div
+      className={tw(
+        'flex flex-col items-center justify-center h-screen text-red-600 text-2xl'
+      )}>
       <h1>Hello World</h1>
     </div>
   )
