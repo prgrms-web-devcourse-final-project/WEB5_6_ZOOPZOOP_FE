bb63a0929cf1d505ede037aef4f028ffbb123610
echo <play3step@naver.com>
2025-09-22 12:25:25 +0900

    [style] 스페이스 생성 이름 입력 모달UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { tw } from '@/shared/lib'
-import { ModalLayout } from '@/shared/ui/modal/ModalLayout'
+import { CreateSpaceModal } from '@/shared/ui/modal/CreateSpaceModal'
 
 export default function Home() {
   return (
-    <div className={tw('flex-center h-screen  text-2xl')}>
-      <ModalLayout size="md">
-        <h1>Hello World</h1>
-      </ModalLayout>
+    <div className={tw('flex-center h-screen text-2xl')}>
+      <CreateSpaceModal />
     </div>
   )
 }
