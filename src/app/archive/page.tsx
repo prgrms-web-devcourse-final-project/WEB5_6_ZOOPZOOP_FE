b34c036d71ba85493ae4cf62724a1647a0cd5907
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-09-23 12:19:21 +0900

    [feat] 아카이브 - 헤더 추가

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,3 +1,20 @@
+'use client'
+import { Header } from '@/shared/ui/header'
+import { Button } from '@/shared/ui/header/Header'
+import { Download, Plus } from 'lucide-react'
+
 export default function Archive() {
-  return <div>Archive</div>
+  const buttons: Button[] = [
+    { label: '파일 생성', icon: Plus, onClick: () => {} },
+    { label: '파일 업로드', icon: Download, onClick: () => {} }
+  ]
+  return (
+    <div>
+      <Header
+        title="아카이브"
+        buttons={buttons}
+        searchBar={{ placeholder: '검색어를 입력해 주세요' }}
+      />
+    </div>
+  )
 }
