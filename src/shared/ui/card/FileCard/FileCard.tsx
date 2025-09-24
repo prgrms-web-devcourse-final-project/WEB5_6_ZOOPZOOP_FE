5fbd6a5f11baa9bd9be568215667d25ef92899f1
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-09-24 15:20:11 +0900

    [design] 파일 카드, 테이블의 CategoryTag -> Badge로 통일

--- src/shared/ui/card/FileCard/FileCard.tsx
+++ src/shared/ui/card/FileCard/FileCard.tsx
@@ -7,6 +7,7 @@ import { Avatar } from '../../avatar'
 
 import FileCardTitle from './FileCardTitle'
 import FileCardThumbnail from './FileCardThumbnail'
+import { Badge } from '../../badge'
 
 interface Props {
   category: string // 태그
@@ -62,10 +63,7 @@ const FileCard = ({
       />
       {/* 카테고리 + 메뉴 */}
       <div className="flex justify-between">
-        <CategoryTag
-          name={category}
-          color="bg-blue-200"
-        />
+        <Badge name={category} />
         <button
           type="button"
           className="text-gray-normal  hover:text-gray-dark cursor-pointer"
