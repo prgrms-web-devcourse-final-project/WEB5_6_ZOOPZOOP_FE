86c985556097e6e4640630f9cda4673069254393
Jaden <hyunbin.kim.3376@gmail.com>
2025-09-25 15:52:10 +0900

    [feat] oauth callback 페이지 임시 완성

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,16 +1,41 @@
 'use client'
 
-// import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 const AuthCallbackPage = () => {
-  // const router = useRouter()
-  // const searchParams = useSearchParams()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
-    // console.log('안녕?')
-  }, [])
+    const isSuccess = searchParams.get('success')
 
-  return <div>Callback auth</div>
+    if (isSuccess) {
+      setMessage('로그인 성공! 잠시간 기다려주세요..')
+
+      // 유저 정보 전역 상태 저장
+
+      // 저장 후 리다이랙트
+      setTimeout(() => {
+        router.push('/news')
+      }, 2000)
+    } else {
+      setMessage('로그인 실패..')
+
+      setTimeout(() => {
+        router.push('/auth/login')
+      }, 2000)
+    }
+  }, [searchParams, router])
+
+  return (
+    <section className="h-screen flex-center">
+      <div className="flex-center flex-col gap-4">
+        <div className="size-32 rounded-full border-16 border-gray-400 border-l-green-normal animate-spin"></div>
+        <span className="text-xl font-medium text-center">{message}</span>
+      </div>
+    </section>
+  )
 }
 export default AuthCallbackPage
