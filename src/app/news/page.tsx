4ca781e132f77187f4068f7bbf356e9adbc7d456
echo <play3step@naver.com>
2025-09-26 12:26:46 +0900

    [feat] 주요 뉴스 데이터 추가

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,10 +1,8 @@
 import { fetchNews } from '@/entities/news'
 import { SearchSection } from '@/features/news-search'
-import Pagination from '@/shared/ui/pagination/Pagination'
-import { MajorNews } from '@/widgets/news-major/ui/MajorNews'
+import { MajorNews } from '@/widgets/news-major'
 import { NewsGrid } from '@/widgets/news-section'
 import { Metadata } from 'next'
-import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: '뉴스',
@@ -15,16 +13,13 @@ export default async function News() {
   const news = await fetchNews()
   return (
     <div className="w-full flex flex-col p-10 min-h-[calc(100vh-72px)]">
-      <div className="flex-1">
-        <h1 className="text-2xl font-bold mb-6">뉴스</h1>
+      <div className="flex-1 flex flex-col gap-5">
         <MajorNews />
-        <SearchSection />
+        <div>
+          <h1 className="text-2xl font-bold mb-6">뉴스 키워드</h1>
+          <SearchSection />
+        </div>
         <NewsGrid news={news.data.items} />
-      </div>
-      <div className="flex justify-center mt-6">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Pagination totalPages={5} />
-        </Suspense>
       </div>
     </div>
   )
