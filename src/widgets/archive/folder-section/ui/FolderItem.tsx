b84b3093c3ffe612a0958e5ce42c9e7cccdee89a
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-09-27 13:58:22 +0900

    [design] 아카이브 - 폴더 섹션, 선택된 경우 ui 활성화

--- src/widgets/archive/folder-section/ui/FolderItem.tsx
+++ src/widgets/archive/folder-section/ui/FolderItem.tsx
@@ -5,9 +5,10 @@ import Link from 'next/link'
 interface Props {
   folderName: string
   isUndo: boolean
+  isActive: boolean
 }
 
-function FolderItem({ folderName, isUndo }: Props) {
+function FolderItem({ folderName, isUndo, isActive }: Props) {
   return (
     <>
       <Link href={!isUndo ? `/archive/${folderName}` : '/archive'}>
@@ -16,7 +17,8 @@ function FolderItem({ folderName, isUndo }: Props) {
             'flex justify-between bg-[#F9FAFB] rounded-sm px-3 py-3 hover:bg-gray-light-active hover:font-bold cursor-pointer',
             isUndo
               ? 'bg-orange-accent text-gray-darker hover:bg-green-normal'
-              : ''
+              : '',
+            isActive ? 'bg-gray-light-active' : ''
           )}>
           <p className="text-base text-gray-darker ">{folderName}</p>
           <button
