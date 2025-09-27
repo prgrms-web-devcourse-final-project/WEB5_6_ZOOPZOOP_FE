d392643e4a1e5be3eda3ea5f5417fe6fb0313766
echo <play3step@naver.com>
2025-09-27 22:11:32 +0900

    [feat] 기본 뉴스 카드 타입 추가 및 react flow 적용

--- src/app/space/dashboard/page.tsx
+++ src/app/space/dashboard/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { BaseNewsCard } from '@/shared/ui/card'
 import {
   Background,
   BackgroundVariant,
@@ -10,9 +11,53 @@ import {
 import '@xyflow/react/dist/style.css'
 
 const initialNodes = [
-  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } }
+  {
+    id: 'n1',
+    type: 'custom',
+    position: { x: 0, y: 0 },
+    data: {
+      label: 'Node 1',
+      image: '/image.png',
+      createdAt: '2025.01.01',
+      content: 'Node 1',
+      title: 'Node 1'
+    }
+  },
+  {
+    id: 'n2',
+    type: 'custom',
+    position: { x: 0, y: 500 },
+    data: {
+      label: 'Node 2',
+      image: '/image.png',
+      createdAt: '2025.01.01',
+      content: 'Node 2',
+      title: 'Node 2'
+    }
+  }
 ]
+
+const nodeTypes = {
+  custom: ({
+    data
+  }: {
+    data: {
+      image: string
+      createdAt: string
+      content: string
+      title: string
+    }
+  }) => (
+    <BaseNewsCard
+      title={data.title}
+      content={data.content}
+      imageUrl={data.image}
+      createdAt={data.createdAt}
+      type="flow"
+    />
+  )
+}
+
 const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }]
 
 export default function Page() {
@@ -22,10 +67,12 @@ export default function Page() {
         <ReactFlow
           nodes={initialNodes}
           edges={initialEdges}
+          nodeTypes={nodeTypes}
           fitView>
           <MiniMap position="top-right" />
 
           <Controls />
+
           <Background
             variant={BackgroundVariant.Dots}
             gap={12}
