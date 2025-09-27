abf77710768e6b95a2575a1cd90afb12393de8cf
Jaden <hyunbin.kim.3376@gmail.com>
2025-09-27 16:44:29 +0900

    [feat] restful 하게 구조 변경

---
+++ src/app/api/user/nickname/route.ts
@@ -0,0 +1,31 @@
+import { updateUserNickname } from '@/entities/user'
+import { createCookieHeader, getAccessToken } from '@/shared/lib'
+import { NextResponse } from 'next/server'
+
+export async function PUT(request: Request) {
+  const payload = await request.json()
+  const token = await getAccessToken()
+
+  if (!token) throw Error('토큰 없음')
+
+  // eslint-disable-next-line no-console
+  console.log('payload', payload)
+
+  const { status, data, msg } = await updateUserNickname(payload, {
+    headers: createCookieHeader(token)
+  })
+
+  if (status !== '200' || !data) {
+    return NextResponse.json({
+      status: 500,
+      data: null,
+      msg: 'api 통신 실패'
+    })
+  }
+
+  return NextResponse.json({
+    status,
+    data,
+    msg
+  })
+}
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,15 +1,16 @@
-import { cookies } from 'next/headers'
-
 import { NextResponse } from 'next/server'
-import { fetchUser } from '@/entities/user'
 
+import { fetchUser } from '@/entities/user'
+import { createCookieHeader, getAccessToken } from '@/shared/lib'
+
+// 사용자 정보 조회
 export async function GET() {
-  const accessToken = (await cookies()).get('accessToken')?.value
+  const token = await getAccessToken()
+
+  if (!token) throw Error('토큰 없음')
 
   const { status, data, msg } = await fetchUser({
-    headers: {
-      Cookie: `accessToken=${accessToken}`
-    }
+    headers: createCookieHeader(token)
   })
 
   if (status !== '200' || !data) {
