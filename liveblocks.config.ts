b62a5e926790c8affcc2e8e7b6cede7b5003ab81
echo <play3step@naver.com>
2025-09-29 21:39:04 +0900

    [feat] liveblocks 마우스 커서 추가

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,42 +1,9 @@
-// Define Liveblocks types for your application
-// https://liveblocks.io/docs/api-reference/liveblocks-react#Typing-your-data
 declare global {
   interface Liveblocks {
-    // Each user's Presence, for useMyPresence, useOthers, etc.
-    Presence: Record<string, never>
-    // Example, real-time cursor coordinates
-    // cursor: { x: number; y: number };
-
-    // The Storage tree for the room, for useMutation, useStorage, etc.
-    Storage: Record<string, never>
-    // Example, a conflict-free list
-    // animals: LiveList<string>;
-
-    // Custom user info set when authenticating with a secret key
-    UserMeta: {
-      id: string
-      info: Record<string, never>
-      // Example properties, for useSelf, useUser, useOthers, etc.
-      // name: string;
-      // avatar: string;
+    // Presence type
+    Presence: {
+      cursor: { x: number; y: number } | null
     }
-
-    // Custom events, for useBroadcastEvent, useEventListener
-    RoomEvent: Record<string, never>
-    // Example has two events, using a union
-    // | { type: "PLAY" }
-    // | { type: "REACTION"; emoji: "🔥" };
-
-    // Custom metadata set on threads, for useThreads, useCreateThread, etc.
-    ThreadMetadata: Record<string, never>
-    // Example, attaching coordinates to a thread
-    // x: number;
-    // y: number;
-
-    // Custom room info set with resolveRoomsInfo, for useRoomInfo
-    RoomInfo: Record<string, never>
-    // Example, rooms with a title and url
-    // title: string;
   }
 }
 
