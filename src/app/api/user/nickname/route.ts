a0da0c3f51378ff3942dbed4e12609b078c5635e
Jaden <hyunbin.kim.3376@gmail.com>
2025-09-29 09:29:33 +0900

    [test] 빌드 테스트

--- src/app/api/user/nickname/route.ts
+++ src/app/api/user/nickname/route.ts
@@ -1,30 +1,33 @@
-import { UpdateNicknameResponse, updateUserNickname } from '@/entities/user'
-import { createCookieHeader, withAuth } from '@/shared/lib/api-route'
+import { updateUserNickname } from '@/entities/user'
+import { createCookieHeader, getAccessToken } from '@/shared/lib/api-route'
+import { NextResponse } from 'next/server'
 
 export async function PUT(request: Request) {
   const payload = await request.json()
+  const token = await getAccessToken()
 
-  return withAuth<UpdateNicknameResponse>(
-    async token =>
-      await updateUserNickname(payload, {
-        headers: createCookieHeader(token)
-      })
-  )
-  // const { status, data, msg } = await updateUserNickname(payload, {
-  //   headers: createCookieHeader(token)
-  // })
+  if (!token) {
+    return NextResponse.json({
+      status: 401,
+      data: null,
+      msg: '인증 토큰이 없습니다'
+    })
+  }
+  const { status, data, msg } = await updateUserNickname(payload, {
+    headers: createCookieHeader(token)
+  })
 
-  // if (status !== '200' || !data) {
-  //   return NextResponse.json({
-  //     status: 500,
-  //     data: null,
-  //     msg: 'api 통신 실패'
-  //   })
-  // }
+  if (status !== '200' || !data) {
+    return NextResponse.json({
+      status: 500,
+      data: null,
+      msg: 'api 통신 실패'
+    })
+  }
 
-  // return NextResponse.json({
-  //   status,
-  //   data,
-  //   msg
-  // })
+  return NextResponse.json({
+    status,
+    data,
+    msg
+  })
 }
