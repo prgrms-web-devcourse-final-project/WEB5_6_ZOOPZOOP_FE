faeb8badac1e2ef310a43c99e16b423bc19f2f03
Jaden <hyunbin.kim.3376@gmail.com>
2025-09-30 16:14:57 +0900

    [feat] 크롬 확장프로그램 callback 페이지 구현

--- src/entities/user/api/user.controller.ts
+++
@@ -1,8 +0,0 @@
-import { httpClient } from '@/shared/lib'
-import { UserResponse } from '../model/type'
-
-export const fetchUser = async (
-  options: RequestInit
-): Promise<UserResponse> => {
-  return httpClient.get<UserResponse>('/api/v1/member/me', options)
-}
