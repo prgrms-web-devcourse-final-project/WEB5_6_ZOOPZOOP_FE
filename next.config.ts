80e153e5766755cf0a8732db3ecf6e78a25a3ae6
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-10-02 12:12:54 +0900

    [chore] next.config.ts 이미지 설정

--- next.config.ts
+++ next.config.ts
@@ -6,28 +6,7 @@ const nextConfig: NextConfig = {
     root: __dirname
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'k.kakaocdn.net'
-      },
-      {
-        protocol: 'http',
-        hostname: 'k.kakaocdn.net'
-      },
-      {
-        protocol: 'http',
-        hostname: 'lh3.googleusercontent.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'zoopzoop-test-bucket.s3.ap-northeast-2.amazonaws.com'
-      }
-    ]
+    unoptimized: true
   }
 }
 
