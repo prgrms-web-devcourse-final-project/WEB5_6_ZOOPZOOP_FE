66d91dbe57d6e499340cb8670fcc0e92fa1626a8
Jaden <hyunbin.kim.3376@gmail.com>
2025-10-05 17:18:56 +0900

    [feat] 닉네임 변경 성공 로직 구현

--- src/features/profile/edit-nickname/model/useEditNickname.ts
+++ src/features/profile/edit-nickname/model/useEditNickname.ts
@@ -1,5 +1,6 @@
 import { useUserStore } from '@/entities/user'
 import { useUpdateNicknameMutation } from '@/entities/user/model/queries'
+import { showErrorToast, showSuccessToast } from '@/shared/ui/toast/Toast'
 import { useState } from 'react'
 
 export const useUpdateNickname = (nickname: string) => {
@@ -10,10 +11,11 @@ export const useUpdateNickname = (nickname: string) => {
   const { isUpdating, updateNickname } = useUpdateNicknameMutation({
     onSuccess: ({ name }) => {
       // 성공 로직
+      showSuccessToast('닉네임 수정 완료')
       updateUser({ name })
     },
-    onError: () => {
-      // 실패 로직
+    onError: error => {
+      showErrorToast(error.message)
     }
   })
 
