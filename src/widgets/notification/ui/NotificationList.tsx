ca48bf2fa31ce0d9604cf498a4a559080e2c35ee
Jaden <hyunbin.kim.3376@gmail.com>
2025-10-08 00:16:02 +0900

    [feat] notification zindex 쌓임 shadcn popover 도입으로 해결

--- src/widgets/notification/ui/NotificationList.tsx
+++ src/widgets/notification/ui/NotificationList.tsx
@@ -3,28 +3,17 @@
 import { useFetchNotifications } from '@/features/notification'
 import NotificationSkeletonList from './NotificationSkeletonList'
 import { NotificationItem } from '@/entities/notification'
-import { useOnClickOutside } from '@/shared/hooks'
-import { RefObject, useRef } from 'react'
 
-interface Props {
-  onClose: () => void
-  triggerRef: RefObject<HTMLButtonElement | null>
-}
-
-const NotificationList = ({ onClose, triggerRef }: Props) => {
-  const notificationRef = useRef<HTMLDivElement>(null)
+const NotificationList = () => {
   const { notifications, isPending } = useFetchNotifications()
-  useOnClickOutside(notificationRef, onClose, true, [triggerRef])
 
   return (
-    <div
-      ref={notificationRef}
-      className="absolute top-5 left-5 shadow-lg w-80 bg-white rounded-lg border border-slate-200 overflow-hidden z-20">
-      <div className="px-4 py-3 border-b border-slate-100">
+    <div>
+      <header className="px-3 py-2 border-b border-slate-100">
         <div className="flex items-center justify-between">
           <h3 className="text-sm font-semibold text-slate-800">알림</h3>
         </div>
-      </div>
+      </header>
       <ul className="max-h-80 overflow-y-auto">
         {isPending ? (
           <NotificationSkeletonList />
@@ -48,7 +37,7 @@ const NotificationList = ({ onClose, triggerRef }: Props) => {
           </>
         )}
       </ul>
-      <div className="px-4 py-2.5 border-t border-slate-100"></div>
+      <footer className="px-4 py-2.5 border-t border-slate-100" />
     </div>
   )
 }
