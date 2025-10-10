16ddbc8ea5fa4453f76e31ec0cd7463955cedce1
echo <play3step@naver.com>
2025-10-10 22:34:56 +0900

    [fix] build 오류 수정

--- src/app/(main)/news/[category]/page.tsx
+++ src/app/(main)/news/[category]/page.tsx
@@ -42,9 +42,9 @@ export default async function NewsCategory({
     <div className="w-full flex flex-col p-10 min-h-[calc(100vh-72px)]">
       <div className="flex-1">
         <h1 className="text-2xl font-bold mb-6">뉴스</h1>
-        {news && news?.data?.items?.length > 0 ? (
+        {news && news?.data?.items && news?.data?.items?.length > 0 ? (
           <NewsGrid
-            news={news.data.items}
+            news={news.data?.items ?? []}
             page={currentPage}
             type="sub"
           />
@@ -54,7 +54,7 @@ export default async function NewsCategory({
       </div>
       <div className="mt-8">
         <Pagination
-          totalPages={news?.data.total ? Math.ceil(news.data.total / 18) : 1}
+          totalPages={news?.data?.total ? Math.ceil(news.data.total / 18) : 1}
         />
       </div>
     </div>
