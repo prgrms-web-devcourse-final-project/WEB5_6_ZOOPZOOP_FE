d43567083a7080c2bb723ef010bfecca926dc0d4
Jaden <hyunbin.kim.3376@gmail.com>
2025-10-13 15:36:56 +0900

    [feat] 리프레시 토큰 발급 운영 테스트

--- src/shared/lib/api-route.ts
+++ src/shared/lib/api-route.ts
@@ -1,7 +1,6 @@
 import { cookies } from 'next/headers'
 import { ACCESS_TOKEN, REFRESH_TOKEN, SESSION_ID } from '../constants'
-import { NextResponse } from 'next/server'
-import { APIResponse, AuthHandler } from '../types/api'
+import { AuthHandler } from '../types/api'
 
 // 엑세스 토큰 추출
 export const getAccessToken = async () => {
@@ -31,24 +30,28 @@ export const createCookieHeader = (
   Cookie: `accessToken=${accessToken}; ${sessionId ? 'sessionId=' + sessionId : ''} `
 })
 
-// 쿠키가 필요한 통신인 경우 => TODO 다 걷어내고 requireAuth로 변경
-export const withAuth = <T>(handler: AuthHandler<T>) => {
-  return async (request: Request): Promise<NextResponse<APIResponse<T>>> => {
-    const token = await getAccessToken()
-    if (!token) {
-      return NextResponse.json({
-        status: 401,
-        data: null,
-        msg: '토큰 없음. 인증 필요'
-      })
-    }
-    return NextResponse.json(await handler(token, request))
-  }
-}
-
 // 쿠키가 필요한 통신인 경우
 export const requireAuth = async <T>(handler: AuthHandler<T>) => {
   const token = await getAccessToken()
   if (!token) return { status: 401, data: null, msg: '토큰 없음, 인증 필요' }
-  return await handler(token)
+  const response = await handler(token)
+  // 엑세스 토큰이 유효하다면 바로 요청 값을 리턴
+  if (response.status !== 401) {
+    // eslint-disable-next-line no-console
+    console.log('404 토큰 다시 해야하는거 ')
+    return response
+  }
 }
+
+// const refreshAccessTokenOnce = async () => {
+//   const refreshUrl = `${process.env.API_URL}/api/v1/auth/refresh`
+//   const sessionId = await getSessionId()
+
+//   const response = fetch(refreshUrl, { method: 'POST', credentials: 'include' })
+
+//   if(!response.ok) {
+//     reutnr {
+
+//     }
+//   }
+// }
