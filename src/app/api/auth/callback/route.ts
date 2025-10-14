6a1b0d96c8c127deddcd9689a0d5978588a033f6
Jaden <hyunbin.kim.3376@gmail.com>
2025-10-14 09:49:15 +0900

    [check] 운영서버 토큰 상태 체크

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -3,54 +3,54 @@ import { getAccessToken } from '@/shared/lib/api-route'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const accessToken = getAccessToken()
-  const url = new URL(request.url)
-
-  if (!accessToken) {
-    return NextResponse.redirect(new URL(PATH.AUTH.LOGIN, url))
-  }
-
-  return NextResponse.redirect(new URL(PATH.NEWS.ROOT, url))
-
-  // const url = new URL(req.url)
-
-  // const accessToken = url.searchParams.get('accessToken')
-  // const refreshToken = url.searchParams.get('refreshToken')
-  // const sessionId = url.searchParams.get('sessionId')
+  // const accessToken = getAccessToken()
+  // const url = new URL(request.url)
 
   // if (!accessToken) {
   //   return NextResponse.redirect(new URL(PATH.AUTH.LOGIN, url))
   // }
 
-  // const response = NextResponse.redirect(new URL(PATH.NEWS.ROOT, url))
+  // return NextResponse.redirect(new URL(PATH.NEWS.ROOT, url))
 
-  // response.cookies.set('accessToken', accessToken, {
-  //   httpOnly: true,
-  //   secure: process.env.NODE_ENV === 'production',
-  //   sameSite: 'lax',
-  //   path: '/',
-  //   maxAge: 60 * 60 // 1시간
-  // })
+  const url = new URL(request.url)
 
-  // if (refreshToken) {
-  //   response.cookies.set('refreshToken', refreshToken, {
-  //     httpOnly: true,
-  //     secure: process.env.NODE_ENV === 'production',
-  //     sameSite: 'lax',
-  //     path: '/',
-  //     maxAge: 60 * 60 * 24 * 30 // 30일
-  //   })
-  // }
+  const accessToken = url.searchParams.get('accessToken')
+  const refreshToken = url.searchParams.get('refreshToken')
+  const sessionId = url.searchParams.get('sessionId')
 
-  // if (sessionId) {
-  //   response.cookies.set('sessionId', sessionId, {
-  //     httpOnly: true,
-  //     secure: process.env.NODE_ENV === 'production',
-  //     sameSite: 'lax',
-  //     path: '/',
-  //     maxAge: 60 * 60 * 24 * 30 // 30일
-  //   })
-  // }
+  if (!accessToken) {
+    return NextResponse.redirect(new URL(PATH.AUTH.LOGIN, url))
+  }
 
-  // return response
+  const response = NextResponse.redirect(new URL(PATH.NEWS.ROOT, url))
+
+  response.cookies.set('accessToken', accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 // 1시간
+  })
+
+  if (refreshToken) {
+    response.cookies.set('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 30 // 30일
+    })
+  }
+
+  if (sessionId) {
+    response.cookies.set('sessionId', sessionId, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 30 // 30일
+    })
+  }
+
+  return response
 }
